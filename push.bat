03a64cc4463903bf46cca9cbc1f03c53d3693bda
ricabhens <ricabhensg@gmail.com>
2022-09-14 15:12:06 +0800

    Act1

--- push.bat
+++ push.bat
@@ -1,4 +1,7 @@
 @echo off
+git remote set-url origin https://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@github.com/ricabhens/CIT142G.git
+pause
 git add .
 git commit -m "Auto Upload"
 git push
+pause
